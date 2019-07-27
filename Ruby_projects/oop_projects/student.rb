7069b647d5c9fde2a28c2652f64f5e486bbce5d0
waspryo <wasp2123321@gmail.com>
2019-07-27 15:31:11 +0900

    Attributes, getters and setters

--- Ruby_projects/oop_projects/student.rb
+++ Ruby_projects/oop_projects/student.rb
@@ -1,16 +1,26 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
+  end
+
 end
 
-def to_s
-  "First name: #{@first_name}"
-end
 
-Ryo = Student.new
-puts Ryo
-# Ryo.first_name = "Leo"
-# puts Ryo
+Melo = Student.new("Khel", "Mihael", "melo", "melo@example.com", "melo1")
+Nia = Student.new("Nate", "River", "Nia", "nia@example.com", "nia1")
+
+puts Melo
+puts Nia
+Melo.last_name = Nia.last_name
+puts "Melo is altered"
+puts Melo
